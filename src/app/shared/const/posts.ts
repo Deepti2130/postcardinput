e66b781f9d25bbc4078f077a552b50cd63fd960c
Deepti2130 <deeptijadhav2130@gmail.com>
2025-09-08 15:56:28 +0530

    posts

--- src/app/shared/const/posts.ts
+++ src/app/shared/const/posts.ts
@@ -10,7 +10,7 @@ export const postsArr:Array<Iposts> = [
   {
     "userId": 1,
     "id": 2,
-    "title": "qui est esse",
+    "title": "Deepti",
     "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
   },
   {
